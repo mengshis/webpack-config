0089ccc3e38fa8855818a4c4173af2ef2017d6d0
v_shimeng <v_shimeng@tencent.com>
2020-05-02 16:17:23 +0800

    chore: update test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    // console.log(baseConfig.entry);
-    assert.equal(baseConfig.entry.index, '/Users/shimeng/webpack-exmple/builder_webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/shimeng/webpack-exmple/builder_webpack/test/smoke/template/src/search/index.js');
+    console.log(baseConfig.entry);
+    assert.equal(baseConfig.entry.index, '/Users/shimeng/webpack-config/test/smoke/template/src/index/index.js');
+    assert.equal(baseConfig.entry.search, '/Users/shimeng/webpack-config/test/smoke/template/src/search/index.js');
   });
 });
