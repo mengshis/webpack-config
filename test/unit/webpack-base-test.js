9fcc477ba01112961624543845ceafaad063daef
v_shimeng <v_shimeng@tencent.com>
2020-05-02 16:23:49 +0800

    chore: update test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
 
   it('entry', () => {
     console.log(baseConfig.entry);
-    assert.equal(baseConfig.entry.index, '/Users/shimeng/webpack-config/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/shimeng/webpack-config/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index, '/home/travis/build/mengshis/webpack-config/test/smoke/template/src/index/index.js');
+    assert.equal(baseConfig.entry.search, '/home/travis/build/mengshis/webpack-config/test/smoke/template/src/search/index.js');
   });
 });
